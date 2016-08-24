78a4bacbb52ae8cf6f040f3957ae29637dbfbbca
Brian Gates <core.safecrlf>
2016-08-24 10:32:46 -0400

    fix number test

--- test/number_test.js
+++ test/number_test.js
@@ -3,7 +3,7 @@ var test = require('tape');
 // a pair of dummy tests to illustrate gulpfile
 
 test('addition', function (t) {
-  t.equal(1 + 0, 2);
+  t.equal(1 + 0, 1);
   t.equal(1 + 1, 2);
   t.end();
 });
